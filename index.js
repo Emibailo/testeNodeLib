88d3fb87ef7637b2972618bc3bb716d88aa3985b
EmiBailo <bailoemily4@gmail.com>
2024-03-11 14:44:40 -0300

    instalar chalk

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('olá mundo');
+import chalk from 'chalk';
+
+console.log(chalk.blue('Hello world!'));
+
+//desencadear métodos texto, cor de fundo e texto em negrito
+console.log(chalk.blue.bgWhite.bold("SENAI"));
+//receber múltiplos argumentos
+console.log(chalk.blue('Curso','de','Node.JS'));
+//métodos aninhados
+console.log(chalk.red('vermelho',chalk.underline.bgBlue('azul')));
+//uso de template e strings
+console.log(`
+CPU: ${chalk.red('90%')}
+RAM: ${chalk.green('40%')}
+DISK: ${chalk.yellow('70%')}
+`);
