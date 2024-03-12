c0c256cd597ad0bde05ef5504ca9e6a37e592a98
EmiBailo <bailoemily4@gmail.com>
2024-03-12 14:53:53 -0300

    segundo commit

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
+import fs from "fs";
+
 import chalk from 'chalk';
 
-console.log(chalk.blue('Hello world!'));
+// console.log(chalk.blue('Hello world!'));
 
-//desencadear métodos texto, cor de fundo e texto em negrito
-console.log(chalk.blue.bgWhite.bold("SENAI"));
-//receber múltiplos argumentos
-console.log(chalk.blue('Curso','de','Node.JS'));
-//métodos aninhados
-console.log(chalk.red('vermelho',chalk.underline.bgBlue('azul')));
-//uso de template e strings
-console.log(`
-CPU: ${chalk.red('90%')}
-RAM: ${chalk.green('40%')}
-DISK: ${chalk.yellow('70%')}
-`);
+// //desencadear métodos texto, cor de fundo e texto em negrito
+// console.log(chalk.blue.bgWhite.bold("SENAI"));
+
+// //receber múltiplos argumentos
+// console.log(chalk.blue('Curso','de','Node.JS'));
+
+// //métodos aninhados
+// console.log(chalk.red('vermelho',chalk.underline.bgBlue('azul')));
+
+// //uso de templates e strings
+// console.log(`
+// CPU: ${chalk.red('90%')}
+// RAM: ${chalk.green('40%')}
+// DISK: ${chalk.yellow('70%')}
+// `);
+
+//2)tratamento de erros, função para tratar erros 
+function trataErro(erro){
+    throw new Error(chalk.red(erro.code,'não há arquivo no diretório. Por favor, verifique o caminho e o diretório novamente'));
+}
+
+//1) escrever uma function que traga arquivos .md
+function pegaArquivo(caminhoDoArquivo){
+    const encoding="utf-8";
+    fs.readFile(caminhoDoArquivo,encoding,(erro,texto)=>{
+        if(erro){
+            trataErro(erro)
+        }
+        console.log(chalk.green(texto));
+    })
+}
+
+pegaArquivo('./arquivos/texto.md');
+//calback(erro,texto)
